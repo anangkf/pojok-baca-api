e38082642622829ce2fc4667307c7c4f061e3bcd
anangkf_ <gonanggoneng@gmail.com>
2023-05-04 10:27:13 +0700

    added Admmin instance

---
+++ src/models/admin.js
@@ -0,0 +1,40 @@
+const {
+  Model,
+} = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Admin extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Admin.init({
+    id: {
+      allowNull: false,
+      primaryKey: true,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    email: {
+      allowNull: false,
+      type: DataTypes.STRING,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
+    },
+    passwordHash: {
+      allowNull: false,
+      type: DataTypes.STRING,
+    },
+  }, {
+    sequelize,
+    modelName: 'Admin',
+  });
+  return Admin;
+};
