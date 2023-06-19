05bf7b8a0939e8425ae3afec968c168fafff5f94
anangkf_ <gonanggoneng@gmail.com>
2023-06-19 15:47:56 +0700

    feat/user: added get user or admin info

--- src/routes/v1/auth.route.js
+++ src/routes/v1/auth.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const { authController } = require('../../controller');
 const Validator = require('../../middleware/Validator');
+const jwtGuard = require('../../middleware/jwtGuard');
 
 const router = express.Router();
 
+router.get('/me', jwtGuard, authController.getInfo);
 router.post('/user/register', Validator('userRegisterSchema'), authController.registerUser);
 router.post('/user/login', Validator('userLoginSchema'), authController.userLogin);
 router.post('/admin/register', Validator('adminAuthSchema'), authController.registerAdmin);
